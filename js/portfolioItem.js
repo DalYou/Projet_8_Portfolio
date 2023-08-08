d9bc47aa7f2dd5a6d2752e193e7cd0b71c21a029
Dalila Younsi <dalilabendellal@yahoo.fr>
2023-08-08 21:38:32 +0200

    portfolio

---
+++ js/portfolioItem.js
@@ -0,0 +1,11 @@
+function portfolioModal (selector) {
+    const element = document.querySelector(selector)
+    const portfolioItems = element.querySelectorAll(".portfolio-item")
+
+    for(let i=0;i<portfolioItems.length;i++){
+        const portfolioItem = porfolioItems[i];
+        portfolioItem.addEventListener("click",e=>{
+            
+        } )
+    }
+}
