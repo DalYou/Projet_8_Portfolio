6927af9d68f82d160f73d9df18002f1946c4522e
Dalila Younsi <dalilabendellal@yahoo.fr>
2023-08-20 17:54:07 +0200

    version finale

--- js/portfolioItem.js
+++ js/portfolioItem.js
@@ -1,11 +1,89 @@
-function portfolioModal (selector) {
-    const element = document.querySelector(selector)
-    const portfolioItems = element.querySelectorAll(".portfolio-item")
+function portfolioModal (selector, portfolioItems) {
+    document.body.innerHTML += `
+    <div id="portfolio-item-modal-container">
+        <div id="portfolio-item-modal">
+            <div id="portfolio-item-modal__close-icon">
+                <i class="bi bi-x"></i>
+            </div>
+            <h2>Titre</h2>
+            <img src="" alt=""/>
+            <p id="portfolio-item-modal__desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, aliquam!</p>
+            <h3>Difficultés rencontrées / moyens de résolution</h3>
+            <div class="accordion" id="accordionDifficulties"></div>
+        </div>
+    </div>
+    `;
+
+    const element = document.querySelector(selector);
+    let innerHTML = `<div class="container-fluid p-0">
+        <div class="row g-0">`;
+
+    const portfolioItemModalContainer = document.getElementById("portfolio-item-modal-container");
+    const closeIcon = document.getElementById("portfolio-item-modal__close-icon");
+
+    const title = portfolioItemModalContainer.querySelector("h2");
+    const img = portfolioItemModalContainer.querySelector("img");
+    const desc = portfolioItemModalContainer.querySelector("p#portfolio-item-modal__desc");
+    const difficulitesList = portfolioItemModalContainer.querySelector("#accordionDifficulties");
+
 
     for(let i=0;i<portfolioItems.length;i++){
-        const portfolioItem = porfolioItems[i];
-        portfolioItem.addEventListener("click",e=>{
-            
-        } )
+        const portfolioItem = portfolioItems[i];
+        const index = i;
+        innerHTML += `
+        <div class="col-lg-4 col-sm-6">
+            <div class="portfolio-item" data-index='${index}'>
+                <div class="card" style="width: 18rem;">
+                    <img src="${portfolioItem.img}" class="card-dalila-img-top" alt="...">
+                    <div class="card-body">
+                        <div class="project-category">${portfolioItem.category}</div>
+                        <a href="">${portfolioItem.link}</a>
+                        <h5 class="card-title">${portfolioItem.title}</h5>
+                        <p class="card-text">${portfolioItem.desc}</p>
+                        <button class="btn btn-primary">En savoir plus</button>
+                    </div>
+                </div>
+            </div>
+        </div>`;
     }
+
+    innerHTML += `</div></div>`;
+    element.innerHTML = innerHTML;
+
+    const portfolioItemElts = document.querySelectorAll(".portfolio-item");
+    for(let i = 0; i < portfolioItemElts.length; i++) {
+        const portfolioItem = portfolioItems[i];
+        portfolioItemElts[i].querySelector("button").addEventListener("click", () => {
+            document.body.style.overflow = 'hidden';
+            portfolioItemModalContainer.style.left = "0";
+            portfolioItemModalContainer.style.top = "0";
+            title.innerText = portfolioItem.title;
+            img.src = portfolioItem.img;
+            desc.innerText = portfolioItem.desc;
+            let innerHTMLCollapse = ``;
+            portfolioItem.difficulties.forEach((difficulty, index) => {
+                innerHTMLCollapse += `
+                <div class="accordion-item">
+                <h2 class="accordion-header">
+                  <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapse-${index}" aria-controls="collapseOne">
+                    ${difficulty}
+                  </button>
+                </h2>
+                <div id="collapse-${index}" class="accordion-collapse collapse" data-bs-parent="#accordionDifficulties">
+                  <div class="accordion-body">
+                   ${portfolioItem.solutions[index]}
+                  </div>
+                </div>
+              </div>`;
+            });
+            difficulitesList.innerHTML = innerHTMLCollapse;
+
+        });
+    }
+
+    closeIcon.addEventListener("click", e => {
+        portfolioItemModalContainer.style.left = null;
+        portfolioItemModalContainer.style.top = null;
+        document.body.style.overflow = null;
+    });
 }
--- js/scripts.js
+++ js/scripts.js
@@ -51,4 +51,124 @@ window.addEventListener('DOMContentLoaded', event => {
         });
     });
 
+    /**** PORTFOLIO ITEMS MODAL ****************/
+    const portfolio = [
+        {
+            title: "Bookie",
+            desc: "Intégration d'une maquette de rendu professionnel en html/css",
+            img: "./assets/img/Booki.png",
+            category: "html/css",
+            link: "https://github.com/DalYou/Booki",
+            difficulties: [
+                "Faire correspondre le design à la maquette fournie.",
+                "Le responsive qui permet que le site soit consulté sur tablette et mobile." 
+            ],
+            solutions: [
+                "En travaillant 4 à 6h par jour sur l'apprentissage de ces 2 langages d'affichage, j'ai finalement réussi l'intégration du projet",
+                "Revoir les cours d'Openclassrooms sur le sujet."
+            ]
+        },
+        {
+            title: "Kasa",
+            desc: "Le but était de créer une application web de location immobilière avec React",
+            img: "./assets/img/Kasa.png",
+            category: "react, js, html/css",
+            link: "https://github.com/DalYou/Projet6_Kasa",
+            difficulties: [
+               "Compréhension des hooks : UseState et UseEffect.",
+               "Se familiariser avec l'utilisations des composants React."
+            ],
+            solutions: [
+                "Beaucoup de recherches en autonomie m'ont permis de saisir les particularités des hooks.",
+                "Beaucoup de recherches en autonomie m'ont permis de saisir les particularités de React et de mener ce projet à son terme."
+            ]
+        },
+        {
+            title: "Mon vieux grimoire",
+            desc: "Développer le back-end d'un site de notation de livres : le vieux grimoire",
+            img: "./assets/img/MonVieuxGrimoire.png",
+            category: "nodejs, express, mongoose, js",
+            link: "https://github.com/DalYou/Projet-7_Books",
+            difficulties: [
+                "Comprendre comment relier le front End au Back End.",
+                "Adapter la structure du projet."
+            ],
+            solutions: [
+                "Des recherches, des vidéos m'ont permis de terminer le projet.",
+                "Consulter des exemples de structures sur des projets existants."
+            ]
+        }
+    ];
+
+    // portfolio modal instance
+    portfolioModal("#portfolio", portfolio);
+
+
+    /**** CONTACT FORM ****************/
+
+    // contact form submit and email sending
+    // with api emailjs
+    emailjs.init("ZBPaerfi0WSuERPI0");
+
+    const formErrorMessages = document.querySelectorAll(".invalid-feedback");
+
+    document.querySelector("#contactForm")
+        .addEventListener("submit", function (e) {
+            e.preventDefault();
+
+            const nameInput     = document.querySelector("#name-form-input").value;
+            const emailInput    = document.querySelector("#email").value;
+            let phoneInput      = document.querySelector("#phone").value;
+            const messageInput  = document.querySelector("#message").value;
+            
+            for(let i = 0, l = formErrorMessages.length; i < l; i++) {
+                formErrorMessages[i].style.display = "none";
+            }
+
+            if(nameInput.trim().length < 2) {
+                document.querySelector("#name-missing").style.display = "block";
+                return;
+            }
+
+            if(!nameInput.match(/^[a-zA-Z' -]+$/)) {
+                document.querySelector("#name-invalid").style.display = "block";
+                return;
+            }
+
+            if(emailInput.trim().length < 2) {
+                document.querySelector("#email-missing").style.display = "block";
+                return;
+            }
+
+            if(!emailInput.match(/[a-z0-9]+@[a-z]+\.[a-z]{2,3}/)) {
+                document.querySelector("#email-invalid").style.display = "block";
+                return;
+            }
+            
+            if(phoneInput.trim().length > 0 && !phoneInput.match(/^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im)) {
+                document.querySelector("#phone-invalid").style.display = "block";
+                return;
+            }
+
+            if(messageInput.trim().length < 5) {
+                document.querySelector("#message-missing").style.display = "block";
+                return;
+            }
+
+            if(!phoneInput) {
+                phoneInput = "non communiqué";
+            }
+
+            const templateParams = {
+               nameInput, emailInput, phoneInput, messageInput
+            };
+
+            emailjs.send('service_lbhd7f1', 'template_0g6gwyu', templateParams)
+                .then(function (response) {
+                    console.log('SUCCESS!', response.status, response.text);
+                }, function (error) {
+                    console.log('FAILED...', error);
+                });
+        });
+
 });
